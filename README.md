eaffc25228335b4e565a8174e607e540995e5941
Dhruv Keyal <dkeyal@umass.edu>
2020-11-28 20:01:27 -0500

    changes to map

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CS520-Final
+# CS520 - Elevation Based Navigation System (EleNA)
+
+EleNA is a mapping tool that provides the ability to minimize or maximize elevation gain for a given path of two locations. The user
--- View/src/index.js
+++ View/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
-import { MDBCol } from "mdbreact";
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGhydXZrZXlhbCIsImEiOiJja2htbmE2NGEwN3YzMzBsZ2I0dWhhZjVhIn0.1QeYm6Ykbj4YH_kcvmLmCg';
 
@@ -34,16 +39,35 @@ class Application extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="overall">
                 <div className='sidebarStyle'>
                     <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
                 </div>
-                {/* <div className='searchStyle'>
-                    <div>Search: {this.state.lng} </div>
+                <div className='searchStyle'>
+                    <TextField
+                        color="white"
+                        label="Start location"
+                        margin="dense"
+                        variant="outlined"
+                    />
                 </div>
-                <MDBCol md="6">
-                    <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-                </MDBCol> */}
+                <div className='searchStyle'>
+                    <TextField
+                        color="white"
+                        label="End location"
+                        margin="dense"
+                        variant="outlined"
+                    />
+                </div>
+                <div className='searchStyle'>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend" style={{color: "black", fontWeight: 30}}>Elevation</FormLabel>
+                        <RadioGroup>
+                            <FormControlLabel value="minimize" control={<Radio />} label="Minimize Elevation" />
+                            <FormControlLabel value="maximize" control={<Radio />} label="Maximize Elevation" />
+                        </RadioGroup>
+                    </FormControl> 
+                </div>
                 <div ref={el => this.mapContainer = el} className='mapContainer' />
             </div>
         )
