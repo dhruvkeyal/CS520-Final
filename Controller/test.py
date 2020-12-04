e1f7fec241f6461077e0ba127b0e893307170406
Shreya <sdsawant@umass.edu>
2020-12-04 16:52:22 -0500

    Modified test cases

--- Controller/test.py
+++ Controller/test.py
@@ -10,25 +10,9 @@ from search import Search
 from dijkstra import Dijkstra
 from shortest import ShortestPath
 from astar import AStar
-import pytest
-from Controller import utils
+import utils
 
-def test_reset_graph():
 
-    Graph = nx.Graph()
-    [Graph.add_node(i, elevation = 0.0) for i in range(5)]
-    edges = [(0,1,1.0), (1,2,1.5), (1,4,2.0), (0,3,4.0), (4,2,3.0)]
-    Graph.add_weighted_edges_from(edges)
-    elevations = [0.0, 0.0,1.0,2.0,3.0]
-
-    for i, elevation in enumerate(elevations):
-        Graph.nodes[i]["elevation"] = elevation
-
-    S = Search(Graph, x = 0.0, elevation_type = "maximize")
-
-    reset_graph = nx.Graph()
-    S.reset_graph(reset_graph)
-    assert S.Graph == reset_graph
 
 # @Test("")
 def test_get_route(S):
@@ -228,14 +212,14 @@ if __name__ == "__main__":
     for i, elevation in enumerate(elevations):
         Graph.nodes[i]["elevation"] = elevation
    
-    S = Search(Graph, x = 0.0, elevation_type = "maximize")
+    S = Search(Graph, x = 0.0, start = 0, end = 2, elevation_type = "maximize")
     test_get_route(S)
 
-    test_get_cost(Graph)
+    # test_get_cost(Graph)
     test_get_elevation(Graph)
 
-    S = ShortestPath(Graph, x = 50.0, elevation_type="maximize")
-    test_get_shortest_distance(S)
+    # S = ShortestPath(Graph, x = 50.0, elevation_type="maximize")
+    # test_get_shortest_distance(S)
 
    
 
