e8c108658e0ff69816f97caae69be2c31e6c936b
ajana13 <ajana@umass.edu>
2020-12-04 14:29:55 -0500

    Added internal documentation for search.py

--- Controller/search.py
+++ Controller/search.py
@@ -7,9 +7,11 @@ import utils
 
 '''
 Class for the path search between two nodes.
+This is the super class, abstraction for extensibility.
+Dijkstra and AStar exdend this implementation.
 '''
 class Search:
-
+    # Constructor for Search objects 
     def __init__(self, Graph, x=0.0, elevation_type="maximize"):
         self.Graph = Graph
         self.x = x
@@ -35,9 +37,9 @@ class Search:
     Return: The best route between two points.
     '''
     def get_route(self, parent_node, end):
-        
         path = [end]
         curr = parent_node[end]
+        # backtrack until the start is reached to find the route
         while curr!=-1:
             path.append(curr)
             curr = parent_node[curr]
@@ -58,14 +60,6 @@ class Search:
     def found_end(self, parent_node, cost):
         route = self.get_route(parent_node, self.end_node)
         elevation_dist, drop_distance = utils.get_elevation(self.Graph, route, "elevation_gain"), utils.get_elevation(self.Graph, route, "elevation_drop")
+        # updates the best found route/cost and data
         self.best = [route[:], cost, elevation_dist, drop_distance]       
 
-
-# graph = pd.read_pickle('../Model/map.p')
-
-# s = [42.3868, -72.5301]
-# e = [42.22560, -72.31122]
-# graph = g.get_graph(s,e)
-# a = Search(graph)
-# a.dijkstra()
-# print(a.best) 
